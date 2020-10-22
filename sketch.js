9d99dffa66ba4461ae98f3a5b97305eee2e11e49
ojasnakade <72932558+ojasnakade@users.noreply.github.com>
2020-10-22 20:58:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var rect1,rect2
+
+function setup() {
+  createCanvas(1200,800);
+  
+  rect1=createSprite(400, 200, 50, 80);
+  rect1.shapeColor="blue";
+
+  rect2=createSprite(600,400,80,50);
+  rect2.shapeColor="blue";
+
+}
+
+function draw() {
+  background(0);
+  
+  rect2.x=mouseX;
+  rect2.y=mouseY;
+
+  if(rect2.x-rect1.x<rect2.width/2+rect1.width/2&&
+    rect1.x-rect2.x<rect2.width/2+rect1.width/2&&
+    rect2.y-rect1.y<rect2.height/2+rect1.height/2&&
+  rect1.y-rect2.y<rect2.height/2+rect1.height/2){
+    rect1.shapeColor="red";
+    rect2.shapeColor="red";
+  }
+
+  else{
+    rect1.shapeColor="blue";
+    rect2.shapeColor="blue";
+  }
+  
+
+  drawSprites();
+}
